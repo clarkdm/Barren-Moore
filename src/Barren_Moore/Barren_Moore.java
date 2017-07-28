6dd54fb084fa2a066330c3d777502ba7c2d8e2d6
clarkdm <dmclark@hotmail.co.uk>
2017-07-28 12:05:17 +0100

    add events, plater and map

--- src/Barren_Moore/Barren_Moore.java
+++ src/Barren_Moore/Barren_Moore.java
@@ -1,5 +1,6 @@
 package Barren_Moore;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -7,14 +8,23 @@ import java.util.Scanner;
  */
 public class Barren_Moore {
 
-    Map start = new Map(1, "You awaken to find yourself in a barren moor. ", "look", 0, 0);
-    Map end = new Map(1, "Grey foggy clouds float oppressively close to you,\n" +
-            "reflected in the murky grey water which reaches up your shins.\n" +
-            "Some black plants barely poke out of the shallow water.\n", " \"north\",\"south\",\"east\",or \"west\"", 3, 5);
-
+    Event start = new Event(1, "You awaken to find yourself in a barren moor. "
+            , "look", 0, 0);
+    Event end = new Event(1,"You see a box sitting on the plain.   Its filled " +
+            "with treasure!  You win!  The end.","",3, 5);
+    Event curint=null;
+ArrayList<Event> map = new ArrayList<Event>();
     Player p;
 
     public Barren_Moore() {
+        map.add(start);
+        map.add(end);
+        start.setLeft(new Event(2, "Grey foggy clouds float oppressively close " +
+                "to you,\n" +
+                "reflected in the murky grey water which reaches up your shins.\n" +
+                "Some black plants barely poke out of the shallow water.\n",
+                " \"north\",\"south\",\"east\",or \"west\"", 0, 0)
+    );
     }
 
     public String get_input_output(String output) {
@@ -39,15 +49,37 @@ public class Barren_Moore {
         p = new Player(get_input_output("name"));
         output("hallo " + p.getName());
     }
+    public double the_dial_reads(Event e) {
+      double temp = Integer.MAX_VALUE;
+      int index = 0;
+        for (int i =0;i<map.size();i++){
+            double x = dial_reads(map.get(i));
+            if (x<temp){
+                index = i;
+            }
+        }
 
-    public int the_dial_reads() {
-        p_lat = p.getLat();
-        p_lon = p.getLon();
+        return temp;
 
 
 
     }
-//    public void get(String output){
+    public double dial_reads(Event e) {
+        int p_lat = p.getLat();
+        int p_lon = p.getLon();
+        int m_lat = e.getLat();
+        int m_lon = e.getLon();
+        double a = Math.abs(m_lat-p_lat) ;
+        a=a*a;
+        double b = Math.abs(m_lon-p_lon);
+        b=b*b;
+
+        return Math.sqrt(a+b);
+
+
+
+    }
+//    public void pythagoras(String output){
 //
 //    }
 //    public void get(String output){
---
+++ src/Barren_Moore/Event.java
@@ -0,0 +1,135 @@
+package Barren_Moore;
+
+/**
+ * Created by dmclark on 28/07/17.
+ */
+public class Event {
+    private Event left;
+    private Event right;
+    private int id = 0;
+    private String info = "";
+    private String Try = "";
+    private int lat = 0;
+    private int lon = 0;
+    boolean pik = false;
+    String Question = "";
+    String choices_L = "L";
+    String choices_R = "R";
+    boolean dune =false;
+
+
+    public Event(int id, String info, String aTry, int lat, int lon) {
+        this.id = id;
+        this.info = info;
+        Try = aTry;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public Event(Event left, Event right, int id, String info, String aTry,
+                 int lat, int lon, String question, String choices_L, String choices_R) {
+        this.left = left;
+        this.right = right;
+        this.id = id;
+        this.info = info;
+        Try = aTry;
+        this.lat = lat;
+        this.lon = lon;
+        Question = question;
+        this.choices_L = choices_L;
+        this.choices_R = choices_R;
+    }
+
+    public Event(Event left, int id, String info, String aTry, int lat, int lon) {
+        this.left = left;
+        this.id = id;
+        this.info = info;
+        Try = aTry;
+        this.lat = lat;
+        this.lon = lon;
+
+    }
+
+    public boolean isDune() {
+        return dune;
+    }
+
+    public void setDune(boolean dune) {
+        this.dune = dune;
+    }
+
+    public String getTry() {
+        return Try;
+    }
+
+    public boolean isPik() {
+        return pik;
+    }
+
+    public String getQuestion() {
+        return Question;
+    }
+
+    public String getChoices_L() {
+        return choices_L;
+    }
+
+    public String getChoices() {
+        return choices_L + "\n" + choices_R;
+    }
+
+    public String getChoices_R() {
+        return choices_R;
+    }
+
+
+
+    public Event getLeft() {
+        return left;
+    }
+
+    public void setLeft(Event left) {
+        this.left = left;
+    }
+
+    public Event getRight() {
+        return right;
+    }
+
+    public void setRight(Event right) {
+        this.right = right;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public int getLat() {
+        return lat;
+    }
+
+    public int getLon() {
+        return lon;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "left=" + left +
+                ", right=" + right +
+                ", id=" + id +
+                ", info='" + info + '\'' +
+                ", Try='" + Try + '\'' +
+                ", lat=" + lat +
+                ", lon=" + lon +
+                ", pik=" + pik +
+                ", Question='" + Question + '\'' +
+                ", choices_L='" + choices_L + '\'' +
+                ", choices_R='" + choices_R + '\'' +
+                '}';
+    }
+}
